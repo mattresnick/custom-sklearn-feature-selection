2dc7931eaa3f02ce3e1d3bedd78d1ee1d29e74f2
mattresnick <53015317+mattresnick@users.noreply.github.com>
2020-04-02 17:50:27 -0600

    Efficiency/documentation updates
    Some minor updates for efficiency of certain lines, as well as added documentation.

--- FSA.py
+++ FSA.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.estimator_checks import check_estimator
 
@@ -8,6 +9,37 @@ from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
 
 
 class FSAEstimator(BaseEstimator, ClassifierMixin):
+    '''
+    Estimator class for Feature Selection with Annealing. This will both select
+    features for best prediction, as well as train weights for the prediction to
+    be performed.
+    
+    Parameters:
+        - kvars: List containing integer values. Each value for k is the target
+        number of features.
+        
+        - N_iter: Integer valued number of iterations to be performed. Stopping
+        criterion can be specified during call to training step.
+        
+        - Mu: Floating point or integer value. Model constant.
+        
+        - s: Floating point or integer value. Model constant.
+        
+        - eta: Floating point or integer value. Model constant/learning rate.
+        
+        - weight_sets: List of float ndarrays. Existing model weights. Should be 
+        left empty unless using values from previously trained model.
+        
+        - loss_sets: List of float ndarrays. Loss values from previous training 
+        run. Should be left empty unless using values from previously trained model.
+        
+        - SI_sets: List of integer ndarrays. Selected indices from a feature
+        set from a previous run. Should be left empty unless using values from 
+        previously trained model.
+        
+        - best_run_ind: Integer. Selected best run index from previously 
+        trained model.
+    '''
     
     def __init__(self, kvars=[0], N_iter=500, mu=5, s=0.001, eta=1,
                  weight_sets=[], loss_sets=[], SI_sets=[], best_run_ind=0):
@@ -23,46 +55,95 @@ class FSAEstimator(BaseEstimator, ClassifierMixin):
         self.best_run_ind = best_run_ind
     
     def sigmoid(self, x):
+        '''
+        Simple Logistic function.
+        '''
         return 1/(1+np.exp(-x))
     
     def lorenzLoss(self, N, X, y, beta):
-        ybx = np.array(np.multiply(y, np.matmul(X, beta))) 
-        logic_yield1 = np.array([1 if ybx[i]<=1 else 0 for i in range(len(ybx))])
-        logic_yield2 = np.log(1+np.square(ybx-1))
+        '''
+        Lorenz loss as described in FSA paper (Barbu et. Al.)
         
-        result = (1/N)*sum(np.multiply(logic_yield1,logic_yield2)) + self.s*np.linalg.norm(beta, ord=2)**2
+        Paramters:
+            - N: Integer, number of samples.
+            - X: Array-like, normalized floating point values. Input samples.
+            - y: Float or integer ndarray. Input target values.
+            - beta: Array-like, floating point values. Trainable weights.
+        Returns:
+            - Floating point value indicating loss.
+        '''
+        ybx = y*(X@beta)        
+        result = (1/N)*sum(np.array([int(i<=1) for i in ybx])*np.log(1+np.square(ybx-1))) + \
+        self.s*np.linalg.norm(beta, ord=2)**2
         
         return result
 
     def invSchedule(self, M, k, i):
+        '''
+        Feature selection schedule as described in FSA paper (Barbu et. Al.)
+        
+        Parameters:
+            - M: Integer, number of features.
+            - k: Integer, number of target features.
+            - i: Integer, current interation of training epoch per k value.
+        Returns:
+            - Integer representing number of features to be kept on the schedule.
+        '''
         Mi = k + (M - k)*max(0, (self.N_iter - 2*i)/(2*i*self.mu + self.N_iter))
         return Mi
     
-    def keyFunc(self, val):
-        return val[0]
-        
-    # Updates the weights -- --
-    def update(self, N, X, y, beta):
     
+    def update(self, N, X, y, beta, l=False):
+        '''
+        Function for updating model weights based on the derivative of the 
+        Lorenz loss function with respect to the weights.
+        
+        Paramters:
+            - N: Integer, number of samples.
+            - X: Array-like, normalized floating point values. Input samples.
+            - y: Float or integer ndarray. Input target values.
+            - beta: Array-like, floating point values. Trainable weights.
+            - l: Boolean, loss is calculated and returned if True.
+        Returns:
+            - beta-like array of updated weight values.
+            - Floating point loss value, returned only if l is True.
+        '''
         # [Nx1] = [Nx1] * [NxM]X[Mx1]
-        ybx = np.array(np.multiply(y, np.matmul(X, beta)))
+        ybx = y*(X@beta)
         
         # [Nx1]
-        logic_yield1 = np.array([1 if ybx[i]<=1 else 0 for i in range(len(ybx))])
+        partial1 = np.array([int(i<=1) for i in ybx])
         
         # [Nx1]
-        logic_yield2 = 2*np.divide((ybx-1),(1 + np.square(ybx-1)))
+        partial2 = 2*np.divide((ybx-1),(1 + np.square(ybx-1)))
         
         # [Mx1] = [MxN]X[Nx1]X[Nx1] + [Mx1]
-        dLdB = (1/N)*np.matmul(X.T, (np.multiply(np.multiply(logic_yield1, logic_yield2),y))) + 2*self.s*beta
+        dLdB = (1/N)*X.T@(partial1*partial2*y) + 2*self.s*beta
         
         beta_new = np.array(beta) - self.eta*dLdB
+        
+        if l:
+            loss = (1/N)*sum(np.array([int(i<=1) for i in ybx])*np.log(1+np.square(ybx-1))) + \
+            self.s*np.linalg.norm(beta, ord=2)**2
+            
+            return beta_new, loss
         
         return beta_new
 
     
     def fit(self, X_o, y, stop_crit=1e-4, verbose=True, sample_weight=None):
+        '''
+        Training/fitting function.
         
+        Parameters:
+            - X_o: Array-like, normalized floating point values. Input samples.
+            - y: Float or integer ndarray. Input target values.
+            - stop_crit: Float or integer, stopping criterion. If the difference
+            in loss value over 50 iterations does not exceed this value, then 
+            training stops.
+            - verbose: Boolean. Will allow progress to be printed to the console
+            if True.
+        '''
         # For sklearn compatibility, validate shapes/labels and store classes
         X_o, y = check_X_y(X_o, y)
         check_classification_targets(y)
@@ -74,39 +155,33 @@ class FSAEstimator(BaseEstimator, ClassifierMixin):
         for k in self.kvars:
             X = X_o
             beta = np.zeros(X.shape[1])
-            num_features = 0
-            save_indices_perm = [i for i in range(len(beta))]
-            M = len(X[0])
-            N = len(X)
+            save_indices_perm = list(range(len(beta)))
+            M = X.shape[1]
+            N = X.shape[0]
             
             loss = []
             for step in range(1,self.N_iter+1):
-                
-                beta = self.update(N, X, y, beta)
+                beta, lloss = self.update(N, X, y, beta, l=True)
                 
                 Mi = int(self.invSchedule(M, k, step))
                 
-                num_features = sum([1 for i in beta])
-                
                 sorter = [[np.abs(beta[i]), save_indices_perm[i], i] for i in range(len(beta))]
-                
-                sorter.sort(key=self.keyFunc, reverse=True)
+                sorter.sort(key=lambda x: x[0], reverse=True)
                 
                 save_indices_perm = [v[1] for v in sorter[:Mi]]
                 save_indices_temp = [v[2] for v in sorter[:Mi]]
                 
-                beta = np.array([beta[v] for v in save_indices_temp])
-                X = np.array([[xo[v] for v in save_indices_temp] for xo in X])
+                beta = beta[save_indices_temp]
+                X = X[:,save_indices_temp]
                 
-                loss.append(self.lorenzLoss(N, X, y, beta))
-                
+                loss.append(lloss)
                 
                 # Stopping criterion.
                 if len(loss)>50 and (loss[-50] - loss[-1])/loss[-50] < stop_crit:
                     break
                 
-                if step%100==0 and verbose:
-                    print('Iteration #%s | # of Features: %s | k: %s ' % (str(step),str(num_features),str(k)))
+                if step%10==0 and verbose:
+                    print('Iteration #%s | # of Features: %s | k: %s ' % (str(step),str(len(beta)),str(k)))
                     
             self.SI_sets.append(save_indices_perm)
             self.weight_sets.append(beta)
@@ -119,31 +194,81 @@ class FSAEstimator(BaseEstimator, ClassifierMixin):
         
         return self
     
-    def predict(self, X):
+    def predict(self, X, k_ind=-1):
+        '''
+        Given input samples, will run the model forward to make predictions using
+        set of "best" weights as determined during the training step. If a value
+        for k_ind is given, the prediction will instead be made from the weights
+        and features from the values of that index.
+        
+        Returns:
+            - Array of integers indicating class prediction for every input sample.
+        '''
         X = check_array(X)
         
+        if k_ind==-1:
+            indices = self.SI_sets[self.best_run_ind]
+            weights = self.w_
+        else:
+            indices = self.SI_sets[k_ind]
+            weights = self.weight_sets[k_ind]
+        
         # Slice out selected features.
-        X = np.array([[xo[v] for v in self.SI_sets[self.best_run_ind]] for xo in X])
+        X = X[:,indices]
         
-        training_yield = np.dot(X, self.w_)
-        preds = np.round(self.sigmoid(training_yield))        
-        #returnpreds = np.array([np.array([p, 0]) for p in preds])                
-        
+        preds = np.round(self.sigmoid(X@weights))
         return preds
     
-    def predict_proba(self, X):
-        #check_is_fitted(self, 'w_')
-        X = check_array(X)
+    def predict_proba(self, X, k_ind=-1):
+        '''
+        Given input samples, will run the model forward to return probabilistic
+        predictions.
         
-        # Slice out selected features.
-        X = np.array([[xo[v] for v in self.SI_sets[self.best_run_ind]] for xo in X])
-        
-        training_yield = np.matmul(X, self.w_)
-        preds = self.sigmoid(training_yield)
-        
+        Returns:
+            - Array of arrays, each indicating probability for each class for
+            every input sample.
+        '''
+        preds = self.predict(X, k_ind=-1)
         prob_out = np.array([np.array([1-p, p]) for p in preds])
         
         return prob_out
+    
+    def performanceTable(self, x_train, x_test, y_train, y_test): 
+        '''
+        Prints a table to console with errors for the model with different
+        number of features.
+        '''
+        check_is_fitted(self, 'w_')
+        
+        print (' ')     
+        print ('   Train Error   |   Test Error   |   k features   ')
+        print ('-----------------+----------------+----------------')
+        
+        for num in range(len(self.kvars)):
+            train_error = (1 - np.mean(self.predict(x_train,num)==y_train))*100
+            test_error = (1 - np.mean(self.predict(x_test,num)==y_test))*100
+            
+            f_kvars = "{:03.0f}".format(self.kvars[num])
+            f_train_error = "{:05.2f}".format(train_error)
+            f_test_error = "{:05.2f}".format(test_error)
+        
+            print ('      ', f_train_error, '    |     ', f_test_error, '    |       ',f_kvars,'     ')
+    
+    def lossPlotter(self, k):
+        '''
+        Plots loss vs training iteration for a given value of k.
+        '''
+        check_is_fitted(self, 'w_')
+        
+        loss_set = self.loss_sets[self.kvars.index(k)]
+        y = np.array(loss_set)
+        x = list(range(len(loss_set)))
+        
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set(xlabel='Iteration Number', ylabel='Loss',
+               title='Loss vs Iteration Number k='+str(k))
+        plt.show()
     
     # Getter function which returns best trained weights and selected indices.
     def getTrainedValues(self):
@@ -155,24 +280,3 @@ class FSAEstimator(BaseEstimator, ClassifierMixin):
     def getFullValues(self):
         check_is_fitted(self, 'w_')
         return self.weight_sets, self.loss_sets, self.SI_sets, self.best_run_ind
-    
-
-def normalize(train_feat, test_feat):
-    
-    train_feat = np.array(train_feat)
-    test_feat = np.array(test_feat)
-    
-    mean=list(np.mean(test_feat,0))
-    stddev=np.std(test_feat,0)
-    
-    zerocols=(stddev!=0)
-    stddev = list(stddev)
-    
-    for val in range(len(zerocols)):
-        if zerocols[val]:
-            train_feat[:,val]=(train_feat[:,val]-mean[val])/stddev[val]
-            test_feat[:,val]=(test_feat[:,val]-mean[val])/stddev[val]
-    
-    return train_feat, test_feat
-
-#print (check_estimator(FSAEstimator))
